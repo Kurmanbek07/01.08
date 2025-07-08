637de87bcf387e51e31a6a276d074747c83907df
Kurmanbek <git@github.com:Kurmanbek07/Menuu.git>
2025-07-08 17:52:56 +0300

    Добавил сборку для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Kurmanbekk/', // имя твоего репозитория
-  plugins: [react()],
+  base: '/01.08/',
+  // ...другие настройки
 })
+
 
