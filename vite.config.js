676fbf93137307983d722be923ae78a15fb87241
Kurmanbek <git@github.com:Kurmanbek07/Menuu.git>
2025-07-08 18:28:44 +0300

    scss added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // относительные пути, чтобы файлы правильно грузились из корня
+  // base: './', // относительные пути, чтобы файлы правильно грузились из корня
   plugins: [react()],
 })
 
